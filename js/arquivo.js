2f9068f103253d5443881ee4621d8493a957f4d2
Heisenbong <164800735+MatheusHenriqueNalinPine@users.noreply.github.com>
2025-07-01 12:23:18 -0300

    Aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,14 @@
+var global = "Variavel global";
+console.log(global);
+
+
+function exemplo2(){
+    var local = "Variavel local";
+    console.log(local);
+}
+
+exemplo2();
+
+function saudacao(){
+    alert("Bem vindo ao nosso site3!");
+}
