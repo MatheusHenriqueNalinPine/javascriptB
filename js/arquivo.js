b2b585b57fcdfbbdc2b88e8e02d91a26226cbaec
Heisenbong <marheushenriquenalinpine@gmail.com>
2025-07-08 12:31:49 -0300

    Aula dia 08/07

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,3 +1,9 @@
+let num1 = Number(prompt("1. Digite o primeiro número: "));
+let num2 = Number(prompt("2. Digite o segundo número: "));
+ alert("Resultado da soma : " + (num1 + num2));
+
+
+
 var global = "Variavel global";
 console.log(global);
 
@@ -7,7 +13,6 @@ function exemplo2(){
     console.log(local);
 }
 
-exemplo2();
 
 function saudacao(){
     alert("Bem vindo ao nosso site3!");
